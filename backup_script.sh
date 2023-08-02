8be6530dbd2171f26136c42f902e12278852bdb2
imenmansouri93 <imennmansourii@gmail.com>
2023-08-02 15:48:55 +0200

    Backup files from container.

--- backup_script.sh
+++ backup_script.sh
@@ -7,16 +7,16 @@ container_name="magical_chandrasekhar"
 container_backup_path="/srv/jupyterhub"
 
 # Replace 'local_backup_path' with the path on your local machine where you want to save the backup files.
-local_backup_path="\Users\USER1\Desktop\backup-data"
+local_backup_path="C:\Users\USER1\Desktop\backup-data"
 
 # Replace 'remote_repository_url' with the URL of your remote repository where you want to push the backup files.
 remote_repository_url="https://github.com/imenmansouri93/cloud-devops.git"
 
 # Step 1: Backup files from the container to the local machine.
-docker cp $magical_chandrasekhar:$/srv/jupyterhub USER1\Desktop\backup-data
+docker cp $magical_chandrasekhar:$/srv/jupyterhub C:\Users\USER1\Desktop\backup-data
 
 # Step 2: Change directory to the backup path on your local machine.
-cd USER1\Desktop\backup-data
+cd $C:\Users\USER1\Desktop\backup-data
 
 # Step 3: Initialize a new Git repository if not already done.
 if [ ! -d ".git" ]; then
