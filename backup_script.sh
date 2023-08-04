b5b625778748dd03e2f263b6a10b44e73b5ffeac
imenmansouri93 <imennmansourii@gmail.com>
2023-08-04 22:47:45 +0200

    Backup files from container.

--- backup_script.sh
+++ backup_script.sh
@@ -6,7 +6,7 @@ container_backup_path="/home/jovyan"
 
 local_backup_path="C:\Users\USER1\Desktop\backup-data"
 
-remote_repository_url="https://github.com/imenmansouri93/imenmansouri.git"
+remote_repository_url="https://github.com/imenmansouri93/mern_Application.git"
 
 docker cp $relaxed_pare:$/home/jovyan /c/Users/USER1/Desktop/backup-data
 
@@ -18,5 +18,5 @@ git add .
 .
 git commit -m "Backup files from container."
 
-git remote add origin https://github.com/imenmansouri93/imenmansouri.git
+#git remote add origin https://github.com/imenmansouri93/mern_Application.git
 git push -u origin master
